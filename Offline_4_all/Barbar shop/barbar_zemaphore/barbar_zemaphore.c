8a757570605abe36f0ae4deceee45455056f14f6
mmk <mmkdhrubo@gmail.com>
2023-02-08 13:13:48 +0600

    added 4 problems

--- Offline_4_all/Barbar shop/barbar_zemaphore/barbar_zemaphore.c
+++ Offline_4_all/Barbar shop/barbar_zemaphore/barbar_zemaphore.c
@@ -12,14 +12,14 @@ zem_t* customer;
 zem_t* mutex;
 int waiting = 0;
 
-void *barber_func(void *arg) {
+void *barberThread(void *arg) {
   int BARBAR_PROCESSING_TIME = *(int*)arg;
   printf("BARBAR PROCESSING TIME: %d\n", BARBAR_PROCESSING_TIME); 
   while (1) {
     zem_down(customer);
     zem_down(mutex);
-    printf("Waiting = %d\n", waiting);
     waiting--;
+    printf("Waiting = %d\n", waiting);
     zem_up(barber);
     zem_up(mutex);
     printf("Barber is cutting hair.\n");
@@ -28,9 +28,8 @@ void *barber_func(void *arg) {
   return NULL;
 }
 
-void *customer_func(void *arg) {
+void *customerThread(void *arg) {
   int customer_id = *(int*)arg;
-  printf("Customer %d entered the shop\n", customer_id);
   zem_down(mutex);
   if (waiting < NUM_CHAIRS) {
     waiting++;
@@ -57,11 +56,11 @@ int main(void) {
   zem_init(mutex, 1);
   int *BARBAR_PROCESSING_TIME = (int*)malloc(sizeof(int));
   *BARBAR_PROCESSING_TIME = 2;
-  pthread_create(&barber_thread, NULL, barber_func, BARBAR_PROCESSING_TIME);
+  pthread_create(&barber_thread, NULL, barberThread, BARBAR_PROCESSING_TIME);
   for (int i = 0; i < NUM_CUSTOMERS; i++) {
     int *customer_id = (int*)malloc(sizeof(int));
     *customer_id = i;
-    pthread_create(&customer_thread[i], NULL, customer_func, customer_id);
+    pthread_create(&customer_thread[i], NULL, customerThread, customer_id);
     int sleeptime = rand()%1 + 0;
     sleep(sleeptime);
   }
@@ -72,7 +71,7 @@ int main(void) {
   pthread_t late_customer_thread; 
   int *customer_id = (int*)malloc(sizeof(int));
   *customer_id = NUM_CUSTOMERS;
-  pthread_create(&late_customer_thread, NULL, customer_func, customer_id);
+  pthread_create(&late_customer_thread, NULL, customerThread, customer_id);
 
   pthread_join(barber_thread, NULL);
   for (int i = 0; i < NUM_CUSTOMERS; i++) {
